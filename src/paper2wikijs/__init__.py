58ff77d038f7f7c104f13db3f82aa21fe714044b
Kuan-Lun Wang <polyphonicared@gmail.com>
2025-07-10 17:35:36 +0800

    Add config module with load_config function and environment variable handling

--- src/paper2wikijs/__init__.py
+++ src/paper2wikijs/__init__.py
@@ -7,11 +7,12 @@ from .service import ScienceDaily2WikiService
 from .sciencedaily_extractor import ScienceDailyExtractor
 from .wikijs_client import WikiJSClient
 from .knowledge_processor import KnowledgeProcessor
+from .config import load_config
 
-__version__ = "0.0.1"
 __all__ = [
     "ScienceDaily2WikiService",
     "ScienceDailyExtractor",
     "WikiJSClient",
     "KnowledgeProcessor",
+    "load_config",
 ]
